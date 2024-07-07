3fbd742fe3507b826551e93555bfe79f4ad55fef
shiva <shiva_05@outlook.com>
2024-07-07 17:32:59 +0530

    test-2

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,24 @@ import mongoose from "mongoose";
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export const connectDb = async () =>{
     await mongoose.connect('mongodb+srv://yashwanth_010906:Yashwanth010906@yashcluster.gpdvhsa.mongodb.net/?retryWrites=true&w=majority&appName=YashCluster')
     .then(()=>{
